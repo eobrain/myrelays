6fccb3ba402926e734db6bfff127fb9807b1fc91
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-07-02 11:08:21 -0700

    Show terms with highest by tf-idf (but not yet updating)

--- index.js
+++ index.js
@@ -51,21 +51,36 @@ function getNotes (relay, callback, eose) {
 }
 
 const SEED_RELAYS = [
-  'wss://relay.n057r.club',
-  'wss://nostramsterdam.vpx.moe',
-  'nostr.bitocial.xyz'
-]
+  'relay.n057r.club',
+  'nostramsterdam.vpx.moe',
+  'relay.mostr.pub',
+  'nostr.sandwich.farm',
+  'nostr.lu.ke',
+  'nostr.1f52b.xyz'
+  // 'nostr.bitocial.xyz'
+].map(host => `wss://${host}`)
 
 const relayIds = []
-let nextRelayId = 0
+let documentCount = 0
 function createRelayId (relay) {
-  relayIds[relay] = `relay${++nextRelayId}`
+  relayIds[relay] = `relay${++documentCount}`
 }
 
 function display (relay, vibe) {
   $relays.insertAdjacentHTML('beforeend',
     `<tr><th>${relay}</th><td>${vibe}</td></tr>`)
 }
+
+const documentsContainingTerm = new Map()
+function documentContainsTerm (relay, term) {
+  if (documentsContainingTerm.has(term)) {
+    documentsContainingTerm.get(term).add(relay)
+  } else {
+    documentsContainingTerm.set(term, new Set([relay]))
+  }
+}
+const inverseDocumentFrequency = (term) =>
+  Math.log(documentCount / documentsContainingTerm.get(term).size)
 
 for (const relay of SEED_RELAYS) {
   createRelayId(relay)
@@ -85,9 +100,12 @@ for (const relay of SEED_RELAYS) {
       } else {
         termCounts[term] = 1
       }
+      documentContainsTerm(relay, term)
     }
   }, () => {
-    const termFrequencies = Object.entries(termCounts).map(([term, count]) => [term, count / totalTermCount]).sort((a, b) => b[1] - a[1])
-    display(relay, termFrequencies.slice(0, 100).map(([term]) => term).join(' '))
+    // term-frequency by inverse-document-frequency
+    const tfIdf = Object.entries(termCounts).map(([term, count]) =>
+      [term, (count / totalTermCount) * inverseDocumentFrequency(term)])
+    display(relay, tfIdf.sort((a, b) => b[1] - a[1]).slice(0, 20).map(([term]) => term).join(' '))
   })
 }
