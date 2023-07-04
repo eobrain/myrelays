3b62f6d79d8bf93393f9defa547db5e44f5e648a
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-07-04 11:12:47 -0700

    Update avoidCount live

--- view.js
+++ view.js
@@ -1,24 +1,26 @@
 /* global $relays $freeze $avoid */
 
-const countIds = []
-const vibeIds = []
+const ids = []
 let relayCount = 0
 
 export const getRelayCount = () => relayCount
 
 export function createRelayId (relay) {
   ++relayCount
-  vibeIds[relay] = `vibe${relayCount}`
-  countIds[relay] = `notes${relayCount}`
+  ids[relay] = {
+    count: `notes${relayCount}`,
+    avoid: `avoid${relayCount}`,
+    vibe: `vibe${relayCount}`
+  }
 }
 
 export function display (relay, count, speed, vibe) {
   count = Math.round(count)
   speed = Math.round(speed * 10)
-  const vibeId = vibeIds[relay]
-  const countId = countIds[relay]
-  const $vibe = document.getElementById(vibeId)
-  const $count = document.getElementById(countId)
+  const id = ids[relay]
+  const $vibe = document.getElementById(id.vibe)
+  const $count = document.getElementById(id.count)
+  const $avoidCount = document.getElementById(id.avoid)
 
   const wordsToAvoid = $avoid.value.split(/\W+/).filter(w => w)
   const words = vibe.split(/\W+/).filter(w => w)
@@ -27,11 +29,17 @@ export function display (relay, count, speed, vibe) {
     avoidCount += wordsToAvoid.includes(word)
   }
   if ($vibe) {
-    $vibe.innerHTML = vibe
     $count.innerHTML = count
+    $avoidCount.innerHTML = avoidCount
+    $vibe.innerHTML = vibe
   } else {
     $relays.insertAdjacentHTML('beforeend',
-      `<tr><th>${relay}</th><td id=${countId}>${count}</td><td>${speed !== undefined ? speed : ''}</td><td>${avoidCount}</td><td id=${vibeId}>${vibe}</td></tr>`)
+      `<tr>
+        <th>${relay}</th>
+        <td id=${id.count}>${count}</td>
+        <td>${speed !== undefined ? speed : ''}</td>
+        <td id=${id.avoid}>${avoidCount}</td>
+        <td id=${id.vibe}>${vibe}</td></tr>`)
   }
   sortRelays()
 }
